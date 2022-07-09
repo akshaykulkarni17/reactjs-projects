fce187cad4093bb42cd3bfeadb27dbe6a0c5399f
akshaykulkarni17 <akshaykulkarni17@gmail.com>
2022-07-09 14:01:56 +0530

    Added blogposts

---
+++ blogposts/src/actions/index.js
@@ -0,0 +1,14 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPosts =  () =>  async dispatch => {
+        const response =  await jsonPlaceholder.get('/posts');
+        dispatch({type: 'FETCH_POSTS', payload: response.data});
+};
+
+export const fetchUser = userId => dispatch => _fetchUser(userId,dispatch);
+
+const _fetchUser = _.memoize( async (userId,dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+    dispatch({type: 'FETCH_USER', payload: response.data});
+});
